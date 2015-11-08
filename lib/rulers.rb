aa23b882b185358a3b8840f994c156274d79f6ea
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-11-08 17:58:03 +0100

    exercise chapter 2 raising exception when bad requests,

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,14 +7,18 @@ module Rulers
     def call(env)
       `echo debug > debug.txt`;
 
-      if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Type' => 'text/html'}, []]
-      end
+      begin
+        if env['PATH_INFO'] == '/favicon.ico'
+          return [404, {'Content-Type' => 'text/html'}, []]
+        end
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue
+        [500, {'Content-Type' => 'text/html'}, ["It's is a bad route!"]]
+      end
     end
   end
 
